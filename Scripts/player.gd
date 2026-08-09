61ec16ddfc7a4f7c28aeba028d9c06794f225363
ajroa05 <armandjohnroa@gmail.com>
2026-08-09 11:12:53 +1000

    added movement to player node
    export var of speed

--- Scripts/player.gd
+++ Scripts/player.gd
@@ -1,5 +1,19 @@
 extends CharacterBody2D
 
+@export var speed : int = 300
 
 func _physics_process(delta: float) -> void:
+	velocity = Vector2.ZERO
+	
+	if Input.is_action_pressed("move_left"):
+		velocity.x -= 1
+	if Input.is_action_pressed("move_right"):
+		velocity.x += 1
+	if Input.is_action_pressed("move_up"):
+		velocity.y -= 1
+	if Input.is_action_pressed("move_down"):
+		velocity.y += 1
+		
+	velocity *= speed
+	
 	move_and_slide()
